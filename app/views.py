e295cab0ac1c352ca8266af91fcb7b2492e0ad4b
Denis Kolosov <denis.kolosov@gmail.com>
2015-10-28 19:17:46 +0300

    Add macros for modals, changed theme

--- app/views.py
+++ app/views.py
@@ -5,3 +5,23 @@ from app import app
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html")
